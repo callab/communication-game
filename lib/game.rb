346266b9dfac8c04b395111f9cffc8052c1f3a30
Sinclair Target <sinclairtarget@gmail.com>
2019-04-17 09:57:40 -0500

    Handle client input in a more async fashion. Register socket listeners once.

--- lib/game.rb
+++ lib/game.rb
@@ -6,29 +6,40 @@ class Game
     @player_two = player_two
     @current_player = player_one
     @board = Board.new(4, 4)
+
+    @player_one.socket.on(:message) do |event|
+      handle_input(@player_one, event)
+    end
+
+    @player_two.socket.on(:message) do |event|
+      handle_input(@player_two, event)
+    end
   end
 
   def start
-    round
+    begin_round
   end
 
-  def round
+  def begin_round
     send_players @board.draw
 
     @current_player.send "Player #{@current_player.id}, it is your turn."
     @current_player.send "Enter coordinates to scan as x, y:"
-
-    @current_player.socket.on(:message) do |event|
-      x, y = event.data.split(',')
-      @board.scan(x.to_i, y.to_i, @current_player.id)
-      swap_players
-      round
-    end
   end
 
   def send_players(message)
     @player_one.send(message)
     @player_two.send(message)
+  end
+
+  def handle_input(player, event)
+    message = event.data
+    if player == @current_player
+      x, y = event.data.split(',')
+      @board.scan(x.to_i, y.to_i, @current_player.id)
+      swap_players
+      begin_round
+    end
   end
 
   def swap_players
@@ -37,8 +48,5 @@ class Game
     else
       @current_player = @player_one
     end
-
-    @player_one.socket.on(:message) { }
-    @player_two.socket.on(:message) { }
   end
 end

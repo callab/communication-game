ed968e118b4bfccc87598d58f459d28d5d7c1b03
Sinclair Target <sinclairtarget@gmail.com>
2019-04-24 16:26:37 -0500

    Use typescript for client development.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,19 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/game.js',
+  entry: './src/game.ts',
   output: {
     path: path.resolve(__dirname, '../public'),
     filename: 'bundle.[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: path.join(__dirname, 'src')
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin({

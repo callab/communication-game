c53a7b1198e1f774fd5ad7d90dee554a8e5d2ede
Sinclair Target <sinclairtarget@gmail.com>
2019-04-26 16:06:43 -0500

    Handle tile clicks.

--- client/src/game.ts
+++ client/src/game.ts
@@ -16,30 +16,55 @@ let config = {
   },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 let game = new Phaser.Game(config);
+let map = null;
+let marker = null;
 
 function preload() {
   this.load.image('sky', 'http://labs.phaser.io/assets/skies/space3.png');
 
   this.load.image('grass', 'maps/tilesets/grass-tiles-2-small.png');
-  this.load.image('bushes', 'maps/tilesets/qubodup-bush_0.png');
-  this.load.tilemapTiledJSON('map', '../maps/map.json');
+  this.load.tilemapTiledJSON('map', '../maps/next.json');
 }
 
 function create() {
   this.add.image(400, 300, 'sky');
 
-  const map = this.make.tilemap({ key: 'map' });
-  const groundTileset = map.addTilesetImage('grass', 'grass');
-  const floraTileset = map.addTilesetImage('qubodup-bush_0', 'bushes');
+  map = this.make.tilemap({ key: 'map' });
+  const groundTileset = map.addTilesetImage('grass-tiles-2-small', 'grass');
 
   let x = WIDTH / 2 - map.widthInPixels / 2;
   let y = HEIGHT / 2 - map.heightInPixels / 2;
 
-  const groundLayer = map.createStaticLayer('ground', groundTileset, x, y);
-  const floraLayer = map.createStaticLayer('flora', floraTileset, x, y);
+  const groundLayer = map.createDynamicLayer('ground', groundTileset, x, y);
+
+  marker = this.add.graphics();
+  marker.lineStyle(3, 0xffffff, 1);
+  marker.strokeRect(0, 0, map.tileWidth, map.tileHeight);
+}
+
+function update(time, delta) {
+  let worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
+
+  let pointerTileX = map.worldToTileX(worldPoint.x);
+  let pointerTileY = map.worldToTileY(worldPoint.y);
+
+  marker.x = map.tileToWorldX(pointerTileX);
+  marker.y = map.tileToWorldY(pointerTileY);
+
+  if (this.input.manager.activePointer.isDown) {
+    let tile = map.getTileAt(pointerTileX, pointerTileY, false, 'ground');
+    if (tile) {
+      console.log(`Found tile at (${pointerTileX}, ${pointerTileY}); changing tint!`);
+      tile.tint = 0xff0000;
+    }
+    else {
+      console.log(`Did not find tile at (${pointerTileX}, ${pointerTileY}).`);
+    }
+  }
 }

7df1994ab3881e1f096a2b80da6ade013d75e255
Sinclair Target <sinclairtarget@gmail.com>
2019-06-03 08:06:04 -0500

    Move server-side game configuration into config.js.

--- config.js
+++ config.js
@@ -4,5 +4,11 @@ let env = process.env;
 module.exports = {
   clientBundleName: 'bundle',
   port: env.NODE_ENV == 'production' ? 8080 : 3000,
-  commitTag: env.APP_COMMIT || 'dev'
+  commitTag: env.APP_COMMIT || 'dev',
+  game: {
+    digging_duration_ms: 5 * 1000,
+    game_length_ms: 60 * 1000,
+    fps: 30,
+    players_per_game: 2
+  }
 };
